26ad384289c3f4d5b55f86fb4571bab339107ee4
ahncid <ahncid@gmail.com>
2024-11-23 21:39:12 +0100

    logs

--- src/services/settings.jsx
+++ src/services/settings.jsx
@@ -1,5 +1,5 @@
-export const serverPath = import.meta.env.VITE_BACKEND_URL.replace(/\/+$/, '');
-
+export const serverPath = import.meta.env.VITE_BACKEND_URL.replace(/\/+$/, "");
+console.log("Server Path:", serverPath);
 const settings = {};
 
 export default settings;
